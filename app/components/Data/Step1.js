aaf93b7d1c7e3f650e2f0ec78597c03fe2d5ad3a
NikhilSharma-007 <niks21902@gmail.com>
2024-07-28 15:39:17 +0530

    Updated Step1 and 2

--- app/components/Data/Step1.js
+++ app/components/Data/Step1.js
@@ -7,6 +7,13 @@ const Step1 = [
     allowMultiple: false,
     id: "experience",
     value: "experience",
+    condition: false,
+    conditionValue: [
+      {
+        questionId: "",
+        value: [""],
+      },
+    ],
     options: [
       { value: "1", id: "experience_1" },
       { value: "2", id: "experience_2" },
@@ -18,26 +25,6 @@ const Step1 = [
       { value: "8", id: "experience_8" },
       { value: "9", id: "experience_9" },
       { value: "10", id: "experience_10" },
-    ],
-  },
-  {
-    label: "How likely are you to recommend us to a friend?",
-    fieldType: "radio",
-    simpleSearch: true,
-    allowMultiple: false,
-    id: "recommend",
-    value: "recommend",
-    options: [
-      { value: "1", id: "recommend_1" },
-      { value: "2", id: "recommend_2" },
-      { value: "3", id: "recommend_3" },
-      { value: "4", id: "recommend_4" },
-      { value: "5", id: "recommend_5" },
-      { value: "6", id: "recommend_6" },
-      { value: "7", id: "recommend_7" },
-      { value: "8", id: "recommend_8" },
-      { value: "9", id: "recommend_9" },
-      { value: "10", id: "recommend_10" },
     ],
   },
 ];
--- app/components/Data/Step2.js
+++ app/components/Data/Step2.js
@@ -7,6 +7,60 @@ const Step2 = [
     allowMultiple: true,
     id: "experience_likes",
     value: "experience_likes",
+    condition: true,
+    conditionValue: [
+      {
+        questionId: "experience",
+        value: ["8", "9", "10"],
+      },
+    ],
+    options: [
+      { value: "Effortless Experience", id: "experience_effortless" },
+      { value: "Quick Response", id: "experience_quick" },
+      { value: "Polite", id: "experience_polite" },
+      { value: "User driven", id: "experience_user_driven" },
+      { value: "Practical Approach", id: "experience_practical" },
+      { value: "Satisfactory", id: "experience_satisfactory" },
+    ],
+  },
+  {
+    label: "Please select areas we could do better in.",
+    fieldType: "chicklet",
+    simpleSearch: true,
+    allowMultiple: true,
+    id: "Better_experience",
+    value: "Better_experience",
+    condition: true,
+    conditionValue: [
+      {
+        questionId: "Better",
+        value: ["5", "6", "7"],
+      },
+    ],
+    options: [
+      { value: "Effortless Experience", id: "experience_effortless" },
+      { value: "Quick Response", id: "experience_quick" },
+      { value: "Polite", id: "experience_polite" },
+      { value: "User driven", id: "experience_user_driven" },
+      { value: "Practical Approach", id: "experience_practical" },
+      { value: "Satisfactory", id: "experience_satisfactory" },
+    ],
+  },
+  {
+    label:
+      "Please select areas we could improve and provide you better experience.",
+    fieldType: "chicklet",
+    simpleSearch: true,
+    allowMultiple: true,
+    id: "Improve_experience",
+    value: "Improve_experience",
+    condition: true,
+    conditionValue: [
+      {
+        questionId: "Improve",
+        value: ["1", "2", "3", "4"],
+      },
+    ],
     options: [
       { value: "Effortless Experience", id: "experience_effortless" },
       { value: "Quick Response", id: "experience_quick" },
